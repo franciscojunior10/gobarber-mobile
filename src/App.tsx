1585e8aab9059f3648873cab9e26921276b8ce3f
Francisco Junior <franciscojn49@gmail.com>
2020-07-27 13:27:15 -0300

    feat: configured statusBar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { StatusBar } from 'react-native';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
+    <>
+      <StatusBar backgroundColor="#312e38" barStyle="light-content" />
+    </>
   );
 };
 
